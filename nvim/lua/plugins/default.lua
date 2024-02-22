2ec43871de956cd77a8e28ce0fca63be2631d15a
Adya Farasadi <afarasadi@gmail.com>
2024-02-22 22:00:46 +0700

    add typescript formatters

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -2,4 +2,28 @@ return {
   {
     "ThePrimeagen/harpoon",
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettierd" },
+        ["javascriptreact"] = { "prettierd" },
+        ["typescript"] = { "prettierd" },
+        ["typescriptreact"] = { "prettierd" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
+  },
 }
