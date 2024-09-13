75197c0b689fdfcb97ec7097f0f6d4a6b687f444
Adya Farasadi <afarasadi@gmail.com>
2024-09-13 14:04:34 +0700

    feat(plugins): add luasnip

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+    enabled = true,
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+}
