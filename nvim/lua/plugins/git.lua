211c8a6410ec1d0ba9d4a35a89bd3d1d7af1c839
Adya Farasadi <afarasadi@gmail.com>
2024-11-24 23:40:36 +0700

    feat(git/diffivew): add diff view

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,10 @@
+return {
+  -- add this in ~/.gitconfig
+  -- [merge]
+  -- tool = nvim
+  -- [mergetool "nvim"]
+  -- cmd = nvim -c "DiffviewOpen"
+  -- [mergetool]
+  -- prompt = false
+  { "sindrets/diffview.nvim" },
+}
