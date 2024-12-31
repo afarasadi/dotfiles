14e5e95f49616bbb67e8b566636f250a81ea8445
Adya Farasadi <afarasadi@gmail.com>
2024-12-31 19:50:54 +0700

    fix(nvim/notify): disable notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,18 +1,21 @@
 return {
-  "snacks.nvim",
-  opts = {
-    indent = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = false },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = false }, -- we set this in options.lua
-    toggle = { map = LazyVim.safe_keymap_set },
-    words = { enabled = true },
-  },
+  {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = false },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = false }, -- we set this in options.lua
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = true },
+    },
   -- stylua: ignore
   keys = {
     { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
   },
+  },
+  { "rcarriga/nvim-notify", enabled = false },
 }
