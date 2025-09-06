fc77cb7c32c86a2e58104b8ce3da814fbf464917
afarasadi <afarasadi@gmail.com>
2025-09-07 05:05:10 +0700

    chore(nvim): update
    add some lazy extras + mdc support

--- nvim/lua/config/config.lua
+++ nvim/lua/config/config.lua
@@ -4,4 +4,10 @@ local M = {}
 -- Define the right scratchpad path dynamically based on the current date
 M.vimwiki_today = "~/vimwiki/diary/" .. os.date("%Y-%m-%d") .. ".wiki"
 
+vim.filetype.add({
+  extension = {
+    mdc = "markdown",
+  },
+})
+
 return M
