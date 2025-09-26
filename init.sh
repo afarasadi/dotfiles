6571da9baf641536acb083233b9e574f0b7acaa3
afarasadi <afarasadi@gmail.com>
2025-09-26 10:32:21 +0700

    add init script

---
+++ init.sh
@@ -0,0 +1,20 @@
+# install zi without modifying .zshrc (already added)
+sh -c "$(curl -fsSL get.zshell.dev)" -- -i skip
+
+# Install om-my-posh with brew
+if command -v brew >/dev/null 2>&1; then
+  brew install jandedobbeleer/oh-my-posh/oh-my-posh
+fi
+
+# install tmux plugin manager
+# then enter tmux, source-file ~/.tmux.con
+# then prefix + I to install plugins
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+
+ln -s "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+
+# symlink .files
+chmod +x "$HOME/.dotfiles/symlink-files.sh"
+zsh +x "$HOME/.dotfiles/symlink-files.sh"
+
+source "$HOME/.zshrc"
